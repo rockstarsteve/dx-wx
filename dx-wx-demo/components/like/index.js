6e370ade2127e5df26b79310e6c1f3893f37d3f3
rockstaryao <670139644@qq.com>
2020-04-01 23:13:30 +0800

    测试，基本的熟悉

---
+++ dx-wx-demo/components/like/index.js
@@ -0,0 +1,38 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    like:{
+      type: Boolean,
+      default: false
+    },
+    count:{
+      type: Number,
+      default: 0
+    }
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLike(event){
+      let like = this.properties.like
+      let count = like?this.properties.count-1:this.properties.count+1
+      this.setData({
+        count: count,
+        like: !like
+      })
+
+    }
+  }
+})
