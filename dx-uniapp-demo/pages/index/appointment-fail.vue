a557d9b3cb974984d823675d8a344a10459e6ab6
rockstaryao <670139644@qq.com>
2020-10-14 23:57:32 +0800

    准备使用插件了，为了回滚|个个版本

---
+++ dx-uniapp-demo/pages/index/appointment-fail.vue
@@ -0,0 +1,68 @@
+<template>
+	<view class="container">
+		<image src="../../static/image/index/appointment-fail.png" class="appointment-success" mode=""></image>
+		<view class="appointment-title">
+			预约取号失败
+		</view>
+		<view class="appointment-note">
+			别紧张，试试再取一次！
+		</view>
+		<view class="btn">
+			重新取号
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.container {
+		width: 100vw;
+		height: 100vh;
+		background-image: linear-gradient(#FFFFFF, #F7F7F7);
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		.appointment-success {
+			width: 640rpx;
+			height: 200rpx;
+		}
+		
+		.appointment-title{
+			margin-top: 31rpx;
+			font-size: 30rpx;
+			font-weight: 600;
+			color: #464141;
+		}
+		
+		.appointment-note{
+			margin-top: 21rpx;
+			font-size: 22rpx;
+			font-weight: 400;
+			color: #8B8A8A;
+		}
+		.btn{
+			margin-top: 70rpx;
+			width: 200rpx;
+			height: 70rpx;
+			background: linear-gradient(104deg, #FF9595 0%, #FF4E5B 100%);
+			box-shadow: 0rpx 11rpx 19rpx 0rpx rgba(249, 86, 86, 0.25);
+			border-radius: 40rpx;
+			font-size: 28rpx;
+			font-weight: 400;
+			color: #FFFFFF;
+			text-align: center;
+			line-height: 70rpx;
+		}
+	}
+</style>
--- dx-uniapp-demo/pages/index/appointment-success.vue
+++ dx-uniapp-demo/pages/index/appointment-success.vue
@@ -9,14 +9,41 @@
 		</view>
 		<view class="info-content">
 			<view class="info-header"></view>
-			<view class="info-content-div">
-				<view class="div-top"></view>
-				<view class="div-header">
-					<view class="div-header-line div-header-line-left"></view>
-					<view class="div-header-text">
-						预约信息
+			<view class="info-content-contianer">
+				<view class="info-content-top">
+					<view class="div-top"></view>
+					<view class="div-header">
+						<view class="div-header-line div-header-line-left"></view>
+						<view class="div-header-text">
+							预约信息
+						</view>
+						<view class="div-header-line div-header-line-right"></view>
 					</view>
-					<view class="div-header-line div-header-line-right"></view>
+					<view class="info-line">
+						<view class="line-left">预约类型</view>
+						<view class="line-right">个人现金业务</view>
+					</view>
+					<view class="info-line">
+						<view class="line-left">预约日期</view>
+						<view class="line-right">2020-07-31</view>
+					</view>
+					<view class="info-line">
+						<view class="line-left">预约手机号</view>
+						<view class="line-right">17709876548</view>
+					</view>
+					<view class="cleavage-div"></view>
+				</view>
+				<view class="info-content-bottom">
+					<view class="cleavage-div-line"></view>
+					<view class="cleavage-div"></view>
+					<view class="bottom-title">网点信息</view>
+					<view class="bottom-local">
+						<img src="../../static/image/common/logo_s.png" class="bottom-local-img"  alt="">
+						<view class="bottom-local-text">华夏银行宝山支行网点</view>
+					</view>
+					<view class="bottom-line">地址：上海市宝山区牡丹江路1332-1346号</view>
+					<view class="bottom-line">电话:（021）56110330</view>
+					<view class="bottom-line">营业时间：周一到周五8:30-17:30</view>
 				</view>
 			</view>
 		</view>
@@ -80,12 +107,53 @@
 				border-radius: 16rpx;
 			}
 
-			.info-content-div {
+			.cleavage-div {
+				height: 15rpx;
+				width: calc(100% - 30rpx);
+				height: 15rpx;
+				position: absolute;
+				left: 15rpx;
+			}
+
+			.cleavage-div::before {
+				content: '';
+				position: absolute;
+				width: 15rpx;
+				height: 15rpx;
+				left: -15rpx;
+				
+				
+			}
+			
+			.cleavage-div::after {
+				content: '';
+				position: absolute;
+				width: 15rpx;
+				height: 15rpx;
+				right: -15rpx;
+				
+			}
+			
+			.info-content-top {
 				position: relative;
 				width: 660rpx;
-				height: 670rpx;
+				height: 320rpx;
 				background-color: #FFFFFF;
-				top: -15rpx;
+				margin-bottom: 30rpx;
+
+				.cleavage-div {
+					background: #FFFFFF;
+					bottom: -15rpx;
+					
+				}
+				.cleavage-div::before {
+					background-image: radial-gradient(circle 15rpx at 0 100%, transparent 50rpx, #fff 50%);
+				}
+				.cleavage-div::after {
+					background-image: radial-gradient(circle 15rpx at 100% 100%, transparent 50rpx, #fff 50%);
+				}
+				
+				
 
 				.div-top {
 					width: 100%;
@@ -97,6 +165,7 @@
 					display: flex;
 					justify-content: center;
 					align-items: center;
+					margin-bottom: 10rpx;
 
 					.div-header-line {
 						width: 50rpx;
@@ -122,6 +191,86 @@
 					}
 				}
 
+				.info-line {
+					padding-left: 41rpx;
+					padding-right: 41rpx;
+					display: flex;
+					height: 80rpx;
+					align-items: center;
+
+					.line-left {
+						flex: 5;
+						font-size: 26rpx;
+						font-weight: 400;
+						color: #8B8A8A;
+					}
+
+					.line-right {
+						flex: 10;
+						font-size: 26rpx;
+						font-weight: 600;
+						color: #464141;
+					}
+				}
+			}
+
+			.info-content-bottom {
+				padding-left: 41rpx;
+				position: relative;
+				width: 660rpx;
+				height: 320rpx;
+				background-color: #FFFFFF;
+
+				.cleavage-div {
+					background: #FFFFFF;
+					top: -15rpx;
+				}
+				.cleavage-div::before {
+					background-image: radial-gradient(circle 15rpx at 0 0, transparent 50rpx, #fff 50%);
+				}
+				.cleavage-div::after {
+					background-image: radial-gradient(circle 15rpx at 100% 0, transparent 50rpx, #fff 50%);
+				}
+				.cleavage-div-line{
+					width: 90%;
+					left: 50%;
+					transform: translateX(-50%);
+					position: absolute;
+					border: 2rpx dashed #D7D7D7;
+					top: -14rpx;
+					z-index: 100;
+				}
+				
+				.bottom-title{
+					font-size: 26rpx;
+					font-weight: 400;
+					color: #8B8A8A;
+					line-height: 60rpx;
+				}
+				
+				.bottom-local{
+					line-height: 70rpx;
+					display: flex;
+					align-items: center;
+					.bottom-local-img{
+						width: 35rpx;
+						height: 35rpx;
+					}
+					.bottom-local-text{
+						margin-left: 15rpx;
+						font-size: 28rpx;
+						font-weight: 600;
+						color: #464141;
+					}
+				}
+				
+				.bottom-line{
+					font-size: 22rpx;
+					font-weight: 400;
+					color: #8B8A8A;
+					line-height: 40rpx;
+				}
+				
 			}
 		}
 	}
